aef604b686d2e76449f7f14a445e50ad219cd5bf
Samuel-Brr <samuel.barre972@gmail.com>
2022-06-30 09:34:15 +0200

    implementation of the commandType method on the assembler class

--- build/parser.js
+++ build/parser.js
@@ -1,2 +1,72 @@
 "use strict";
-console.log('TITI');
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Parser = void 0;
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const _ = __importStar(require("lodash"));
+class Parser {
+    constructor(filePath) {
+        this.filePath = filePath;
+        this.readyForUseFileData = this.openTheInputFileAndReturnSanitizedData(this.filePath);
+        for (let i = 0; i < this.readyForUseFileData.length; i++) {
+            let commandType = this.returnCommandType(this.readyForUseFileData[i]);
+            console.log('COMMAND TYPE: ', commandType);
+        }
+    }
+    returnCommandType(command) {
+        if (command.startsWith('@')) {
+            return 'A_COMMAND';
+        }
+        else if (command.startsWith('(')) {
+            return 'L_COMMAND';
+        }
+        else {
+            return 'C_COMMAND';
+        }
+    }
+    //Helper functions ////////////////////////////////////////////////////////
+    openTheInputFileAndReturnSanitizedData(filePath) {
+        const fileData = fs.readFileSync(path.join(__dirname, filePath), 'utf8');
+        const sanitizedFileData = this.sanitizeFileData(fileData);
+        return sanitizedFileData;
+    }
+    sanitizeFileData(data) {
+        let rawFileData = [];
+        rawFileData = data.split('\r\n');
+        _.remove(rawFileData, (ele) => ele.startsWith('//') || ele === '');
+        for (let i = 0; i < rawFileData.length; i++) {
+            if (rawFileData[i].includes('//')) {
+                const toto = rawFileData[i].split('//');
+                toto.splice(1, 1);
+                const stringo = toto.join().trim();
+                rawFileData[i] = stringo;
+            }
+        }
+        return rawFileData;
+    }
+}
+exports.Parser = Parser;
+const test = new Parser('../add.asm');
--- modules/parser.ts
+++ modules/parser.ts
@@ -1,1 +1,70 @@
-console.log('TITI')
+import * as fs from 'fs';
+import * as path from 'path';
+import * as _ from 'lodash';
+
+type Command = 'A_COMMAND' | 'C_COMMAND'| 'L_COMMAND';
+
+export class Parser {
+
+    private readyForUseFileData: string[]
+
+    constructor(
+        private filePath: string,
+
+    ) {
+        this.readyForUseFileData = this.openTheInputFileAndReturnSanitizedData(this.filePath);
+
+        for(let i=0; i<this.readyForUseFileData.length; i++){
+
+            let commandType = this.returnCommandType(this.readyForUseFileData[i])
+            console.log('COMMAND TYPE: ', commandType)
+        }
+    
+    }   
+    
+    private returnCommandType(command: string): Command{
+        if(command.startsWith('@')){
+            return 'A_COMMAND'
+        }else if(command.startsWith('(')){
+            return 'L_COMMAND'
+        }else{
+            return 'C_COMMAND'
+        } 
+    }
+    
+    
+    
+    
+    
+    
+    //Helper functions ////////////////////////////////////////////////////////
+    
+    private openTheInputFileAndReturnSanitizedData(filePath: string) {
+        
+        const fileData = fs.readFileSync(path.join(__dirname, filePath), 'utf8')
+        const sanitizedFileData = this.sanitizeFileData(fileData)
+        return sanitizedFileData        
+    }
+    
+
+    private sanitizeFileData(data: string){
+        
+        let rawFileData = [];
+        rawFileData = data.split('\r\n');
+        _.remove(rawFileData, (ele) => ele.startsWith('//') || ele === '')
+        
+        for(let i=0; i<rawFileData.length; i++){
+            if(rawFileData[i].includes('//')){
+                const toto = rawFileData[i].split('//')
+                toto.splice(1,1)
+                const stringo = toto.join().trim()
+                rawFileData[i] = stringo
+            }
+        }
+
+        return rawFileData
+    }
+
+}
+
+const test = new Parser('../add.asm');
