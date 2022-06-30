fe3350a4c26bc23e545242bfb421961c470e4f85
Samuel-Brr <samuel.barre972@gmail.com>
2022-06-30 16:05:02 +0200

    creation de la jump map

---
+++ build/maps/jump.map.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.jumpMap = void 0;
+exports.jumpMap = new Map();
+exports.jumpMap.set('null', '000');
+exports.jumpMap.set('JGT', '001');
+exports.jumpMap.set('JEQ', '010');
+exports.jumpMap.set('JGE', '011');
+exports.jumpMap.set('JLT', '100');
+exports.jumpMap.set('JNE', '101');
+exports.jumpMap.set('JLE', '110');
+exports.jumpMap.set('JMP', '111');
---
+++ maps/jump.map.ts
@@ -0,0 +1,13 @@
+export const jumpMap = new Map()
+
+jumpMap.set('null', '000') 
+jumpMap.set('JGT', '001') 
+jumpMap.set('JEQ', '010') 
+jumpMap.set('JGE', '011') 
+jumpMap.set('JLT', '100') 
+jumpMap.set('JNE', '101') 
+jumpMap.set('JLE', '110') 
+jumpMap.set('JMP', '111')
+
+
+
