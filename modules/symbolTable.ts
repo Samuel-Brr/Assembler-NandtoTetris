ec8b77d363fb5b343dd1c7d326b30a3f0d389986
Samuel-Brr <samuel.barre972@gmail.com>
2022-07-01 12:30:33 +0200

    Creation of the SymbolTable class + Initialization of the symbol table with all the predefined symbols and their pre-allocated RAM addresses

---
+++ modules/symbolTable.ts
@@ -0,0 +1,33 @@
+export class SymbolTable{
+
+    private symbolTable: Map<string, string> = new Map()
+
+    constructor(){
+        this.symbolTable.set('SP', '0000000000000000')
+        this.symbolTable.set('LCL', '0000000000000001')
+        this.symbolTable.set('ARG', '0000000000000010')
+        this.symbolTable.set('THIS', '0000000000000011')
+        this.symbolTable.set('THAT', '0000000000000100')
+        this.symbolTable.set('R0', '0000000000000000')
+        this.symbolTable.set('R1', '0000000000000001')
+        this.symbolTable.set('R2', '0000000000000010')
+        this.symbolTable.set('R3', '0000000000000011')
+        this.symbolTable.set('R4', '0000000000000100')
+        this.symbolTable.set('R5', '0000000000000101')
+        this.symbolTable.set('R6', '0000000000000110')
+        this.symbolTable.set('R7', '0000000000000111')
+        this.symbolTable.set('R8', '0000000000001000')
+        this.symbolTable.set('R9', '0000000000001001')
+        this.symbolTable.set('R10', '0000000000001010')
+        this.symbolTable.set('R11', '0000000000001011')
+        this.symbolTable.set('R12', '0000000000001100')
+        this.symbolTable.set('R13', '0000000000001101')
+        this.symbolTable.set('R14', '0000000000001110')
+        this.symbolTable.set('R15', '0000000000001111')
+        this.symbolTable.set('SCREEN', '0100000000000000')
+        this.symbolTable.set('KBD', '0110000000000000')
+
+        
+    }
+
+}
